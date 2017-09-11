56113097dc1183920125239bafb6bcda432d2f06
Zane Russell <zaneroosa@gmail.com>
2017-09-11 16:15:24 -0600

    menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Menuoption } from './components/menuoption/Menuoption.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div id="menu">
+          <Menuoption title="Home" class="menu-home" />
+          <Menuoption title="Music" class="menu-music" />
+          <Menuoption title="Coding" class="menu-coding" />
+          <Menuoption title="Contact" class="menu-contact" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <h2>Zane Russell</h2>
+          <h4>Front-end Developer</h4>
+        </div>
       </div>
     );
   }
