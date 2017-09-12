0e748d9c2bafc0cd2d4e595ee5be097494460ac4
Zane Russell <zaneroosa@gmail.com>
2017-09-12 09:47:56 -0600

    redone menu

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import './App.css';
 import { Menuoption } from './components/menuoption/Menuoption.js'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="app">
+        <div className="app-header">
+          <h2 className="app-header-name">Zane Russell</h2>
+          <h4>Front-end Developer</h4>
+        </div>
         <div id="menu">
           <Menuoption title="Home" class="menu-home" />
           <Menuoption title="Music" class="menu-music" />
           <Menuoption title="Coding" class="menu-coding" />
           <Menuoption title="Contact" class="menu-contact" />
-        </div>
-        <div className="App-header">
-          <h2>Zane Russell</h2>
-          <h4>Front-end Developer</h4>
         </div>
       </div>
     );
