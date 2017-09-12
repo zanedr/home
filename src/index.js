6d85dda9420b2301f5b2b416674a7d711c27e1e8
Zane Russell <zaneroosa@gmail.com>
2017-09-12 15:42:58 -0600

    beginning of integrating router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    browserHistory
+  } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+    </Router>
+  ), document.getElementById('app'));
 registerServiceWorker();
