e6df311c50a0d5cba188122d804dcf4dffedb806
Zane Russell <zaneroosa@gmail.com>
2017-09-14 11:49:23 -0600

    added resume section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './About.css';
 
-export const About = () => {
-    return(
-        <div id='about'>
-            <p id='about-text'>
-                I am a Front-End Developer and recent graduate from the Turing School of Software and Design. 
-                This site is both a home page and playground to practice with javascript's interactivity in
-                a simple black and white design environment.
-            </p>
-        </div>
-    )
+export class About extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            resumeDisplay: false,
+        }
+    }
+
+    switchPopUpStatus() {
+        if(this.state.resumeDisplay === true) {
+            this.setState({resumeDisplay: false})
+        } else {
+            this.setState({resumeDisplay: true})
+        }
+    }
+
+    arrow() {
+        if(this.state.resumeDisplay === true) {
+            return(
+                <span>&darr;</span>
+            )
+        } else {
+            return(
+                <span>&uarr;</span>
+            ) 
+        }
+    }
+
+    render() {
+        return(
+            <div id='about'>
+                <p id='about-text'>
+                    I am a Front-End Developer and recent graduate from the Turing School of Software and Design.
+                    Years of being curious about programming and computers led to a transition from teaching
+                    to pursuing an education in front-end engineering.
+                    This site is a resume and playground to experiment with javascript in a simple black 
+                    and white design environment.
+                </p>
+                <button className='about-resume' onClick={() => this.switchPopUpStatus()}>
+                    Resume {this.arrow()}
+                </button>
+            </div>
+        )
+    }
 }
---
+++ src/components/resume/Resume.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Resume.css';
+
+export const Resume = () => {
+    
+}
