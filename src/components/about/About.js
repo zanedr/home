a2a2d03296a1784ec038fc6c754a579487379964
Zane Russell <zaneroosa@gmail.com>
2017-09-23 21:13:22 -0600

    update for cover letter

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -42,24 +42,28 @@ export class About extends Component {
         return(
             <div id='about'>
                 <p className='about-text'>
-                    I am a Front-End Developer and recent graduate from the Turing School of Software and Design.</p>
+                    I am a Front-End Developer and recent graduate from the Turing School of Software and Design with a passion
+                    for learning and problem solving. Like any useful tool, applications should 
+                    allow users to accomplish a task in an intuitive, simple, and effective manner, 
+                    and I strive to write clean, concise, and testable code that reflects that ideal.
+                </p>
                 <p className='about-text'>
-                    In the time before Turing, I was a teacher and working musician.  Music is a chaotic industry, 
-                    and being a member of band requires working effectively as a team and learning new skills 
-                    quickly.
+                    Before Turing, I was a teacher and working musician. 
                     I've managed multiple regionally successful musical groups, taught to all skill levels and ages, 
-                    organized shows and performed across the United States, waited tables, 
+                    organized shows and performed across the United States, 
                     trained new employees, repaired equipment,  
                     and acted as webmaster and social outreach coordinator.
                 </p>
-                <p className='about-text'>These experiences have put me on every side and dynamic of team involvement, from 
+                <p className='about-text'>These experiences have put me on every side and dynamic of team involvement: from 
                     being a team member in a cooperative organization with good-natured folk, to taking the reins and
                     leading the completion of a project, to
                     keeping the peace between volitile personalities to ensure that a commitment is fulfilled.
                 </p>
                 <p className='about-text'>
-                    I'd much rather work with the good-natured folk, and am looking for a role with a team of
-                    great people that enjoy working together.
+
+                </p>
+                <p className='about-text'>
+                    I'm looking for a role with a team of great people that enjoy working together.
                 </p>
                 {/* <p className='about-text'> This site is a homepage, resume, and playground to experiment with javascript in a simple black 
                     and white design environment.
--- src/components/coding/Coding.js
+++ src/components/coding/Coding.js
@@ -22,18 +22,18 @@ export class Coding extends Component {
         if(this.state.popUp === true) {
             return(
                 <div>
-                    <CodingProject title="On Tour" 
-                                   summary="On Tour is a gas calculator for bands on the road. Utilizing the GoogleMaps API, the U.S. Energy Information Administration API, and data provided by the user, On Tour will calculate distances and total cost with respect to local gas prices and return an estimate.  Notes can be stored within each location for easy recall." 
-                                   builtUsing="React, Redux"
-                                   picture={require('./ontour.png')}
-                                   pageLink="https://zanedr.github.io/On-Tour/"
-                                   gitLink="https://github.com/zanedr/On-Tour"/>
                     <CodingProject title="Etude" 
                                    summary="Etude is a music practice assistant created as part of a three person team for our final project at Turing. Etude allows aspiring musicians to speed up or slow down songs to a customized difficulty level while simultaneously displaying the tablature.  Songs can be saved by the user for future use." 
                                    builtUsing="React, Node.js, Express, Knex"
                                    picture={require('./etude.png')}
                                    pageLink="https://shielded-depths-13754.herokuapp.com/"
                                    gitLink="https://github.com/zanedr/Etude"/>
+                    <CodingProject title="On Tour" 
+                                   summary="On Tour is a gas calculator for bands on the road. Utilizing the GoogleMaps API, the U.S. Energy Information Administration API, and data provided by the user, On Tour will calculate distances and total cost with respect to local gas prices and return an estimate.  Notes can be stored within each location for easy recall." 
+                                   builtUsing="React, Redux"
+                                   picture={require('./ontour.png')}
+                                   pageLink="https://zanedr.github.io/On-Tour/"
+                                   gitLink="https://github.com/zanedr/On-Tour"/>
                     <CodingProject title="Tron 2.0" 
                                    summary="Tron 2.0 is a clone of the classic arcade game, Tron.  It has two player capability, variable speeds, and a rudimentary AI to challenge 1-on-1." 
                                    builtUsing="VanillaJS"
